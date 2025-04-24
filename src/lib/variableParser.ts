3b93d1a9abf87cf3cbb637c6ec006f1ebe6da2c1
Saturn Maoudis <saturnmaoudis@protonmail.com>
2025-04-24 13:20:47 +0100

    db variables

--- src/lib/variableParser.ts
+++ src/lib/variableParser.ts
@@ -1,21 +1,30 @@
 import type { Interaction } from "discord.js";
 
-export const parse = (content: string, interaction: Interaction) => {
-    return new Promise<string>((resolve, reject) => {
-        let output = content
-            .replace('#[sender]', interaction.user.displayName)
-            .replace('#[sender:ping]', `<@${interaction.user.id}>`);
+const kvdb = Bun.redis;
 
-        if (content.includes('#[arg:')) {
-            const args = content.matchAll(/#\[arg:(.*?)]/g);
-            for (const arg of args) {
-                // @ts-ignore dw its ok
-                output = output.replace(arg[0], interaction.options.get(arg[1])?.value);
-            }
+export const parse = async (content: string, interaction: Interaction) => {
+    let output = content
+        .replace('#[sender]', interaction.user.displayName)
+        .replace('#[sender:ping]', `<@${interaction.user.id}>`);
+
+    if (content.includes('#[arg:')) {
+        const args = content.matchAll(/#\[arg:(.*?)]/g);
+        for (const arg of args) {
+            // @ts-ignore dw its ok
+            output = output.replace(arg[0], interaction.options.get(arg[1])?.value);
         }
+    }
 
-        resolve(output);
-    });
+    if (content.includes('#[store:')) {
+        const args = content.matchAll(/#\[store:(.*?)]/g);
+        for (const arg of args) {
+            const dbVar = await kvdb.hmget(interaction.guildId as string, [arg[1]]);
+
+            output = output.replace(arg[0], dbVar[0] as string);
+        }
+    }
+
+    return output;
 };
 
 export const variables: { name: string, variable: string, interaction: string; }[] = [
@@ -33,5 +42,10 @@ export const variables: { name: string, variable: string, interaction: string; }
         name: 'Command Argument',
         variable: '#[arg:ARG_NAME]',
         interaction: 'options.get(arg)',
+    },
+    {
+        name: 'Store variable',
+        variable: '#[store:VAR]',
+        interaction: 'n/a',
     }
 ];
